4e82d0bb4f43bc45d97f67db3206b4a9919b245f
Sara Jaramillo <sarasunrise45@gmail.com>
2024-08-06 11:27:50 -0500

    Se agregan modificaciones al header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,19 @@ import '../sass/header.scss';
 const Header = ({links, text}) => {
     return (
         <header>
-            <img alt="Logo" src={logo}></img>
-            <nav>
-             <Links links={links} text="About"/> {/* pass the links array as a prop to Links component */}
-             <Links links={links} text="Careers"/>
-             <Links links={links} text="Events"/>
-             <Links links={links} text="Products"/>
-             <Links links={links} text="Support"/>
-            </nav>
+            <section className="header-section" >
+                <img alt="Logo" src={logo}></img>
+                <nav>
+                    <Links links={links} text="About"/> 
+                    <Links links={links} text="Careers"/>
+                    <Links links={links} text="Events"/>
+                    <Links links={links} text="Products"/>
+                    <Links links={links} text="Support"/>
+                </nav>
+            </section>
+            <section className="header-text" >
+                <h2>IMMERSIVE <br></br> EXPERIENCES <br></br> THAT DELIVER</h2>
+            </section>
         </header>
     );
 }
