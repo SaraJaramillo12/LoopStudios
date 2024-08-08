d2ad4d9d07533859bc56eda9a5c50364dfa3f1e0
Angie Liceth <liceth5262@gmail.com>
2024-08-08 15:10:04 -0500

    modificacion componente gallery-en las imagenes con mobile

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,4 +1,6 @@
 import '../sass/gallery.scss';
+import { useEffect, useState } from 'react';
+// desktop
 import deepearth from '../assets/images/desktop/image-deep-earth.jpg';
 import nightarcade from '../assets/images/desktop/image-night-arcade.jpg';
 import soccerteam from '../assets/images/desktop/image-soccer-team.jpg';
@@ -7,35 +9,62 @@ import fromUp from '../assets/images/desktop/image-from-above.jpg';
 import pocketborealis from '../assets/images/desktop/image-pocket-borealis.jpg';
 import curiosity from '../assets/images/desktop/image-curiosity.jpg';
 import fisheye from '../assets/images/desktop/image-fisheye.jpg';
+// mobile
+import deepEarth from '../assets/images/mobile/image-deep-earth.jpg'
+import nightArcade from '../assets/images/mobile/image-night-arcade.jpg'
+import soccerTeamg from '../assets/images/mobile/image-soccer-team.jpg'
+import Grid from '../assets/images/mobile/image-grid.jpg'
+import FromUp from '../assets/images/mobile/image-from-above.jpg'
+import pocketBorealis from '../assets/images/mobile/image-pocket-borealis.jpg'
+import Curiosity from '../assets/images/mobile/image-curiosity.jpg'
+import Fisheye from '../assets/images/mobile/image-fisheye.jpg'
+
 
 const imagenes = [
-    { title: "DEEP EARTH", image: deepearth },
-    { title: 'NIGH ARCADE', image: nightarcade },
-    { title: "SOCCER TEAM", image: soccerteam },
-    { title: "GRID", image: grid },
-    { title: "FROM UP ABOVE VR", image: fromUp },
-    { title: "POCKET BOREALIS", image: pocketborealis },
-    { title: "THE CURIOSITY", image: curiosity },
-    { title: "MAKE IT FISHEYE", image: fisheye },
+    { title: "DEEP EARTH", image: deepearth, img: deepEarth },
+    { title: 'NIGH ARCADE', image: nightarcade, img: nightArcade},
+    { title: "SOCCER TEAM", image: soccerteam, img: soccerTeamg },
+    { title: "GRID", image: grid, img: Grid },
+    { title: "FROM UP ABOVE VR", image: fromUp, img: FromUp },
+    { title: "POCKET BOREALIS", image: pocketborealis, img: pocketBorealis },
+    { title: "THE CURIOSITY", image: curiosity, img: Curiosity },
+    { title: "MAKE IT FISHEYE", image: fisheye, img: Fisheye},
 ]
 
 const Gallery = () => {
+    const [widthScreen, setWidthScreen] = useState(0);
+
+    useEffect(() => {
+        const handleResize = () => setWidthScreen(window.innerWidth);
+
+        handleResize();
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    const mobileBreakpoint = 400;
+
     return (
         <section className="gallery">
             <div className="gallery__intro">
                 <h2 className="gallery__title">OUR CREATIONS</h2>
                 <button>SEE ALL</button>
             </div>
-            <section className='gallery__grid'>
-            {imagenes.map((imagenes, index) => (
-                <div key={index} className="gallery__item">
-                    <img src={imagenes.image} alt={imagenes.title} />
-                    <h3 className="gallery__item-title">{imagenes.title}</h3>
-                </div>
-            ))}
+            <section className="gallery__grid">
+                {imagenes.map((imagen, index) => {
+                    const imageSrc = widthScreen > mobileBreakpoint ? imagen.image : imagen.img;
+                    return (
+                        <div key={index} className="gallery__item">
+                            <img src={imageSrc} alt={imagen.title} />
+                            <h3 className="gallery__item-title">{imagen.title}</h3>
+                        </div>
+                    );
+                })}
             </section>
         </section>
-    )
-}
+    );
+};
 
 export default Gallery;
